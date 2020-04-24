ad52b792022aeeb9fb4d3063a20c5aea0bc5ff2d
chris west <c@xvxx.io>
2020-04-24 13:13:31 -0700

    cli takes a file and parses it

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 use ldpl::parser::{LDPLParser, Parser, Rule};
 
 fn main() {
-    // let pairs = LDPLParser::parse(Rule::program, r#"hi mrs mom"#).unwrap();
-    let pairs = LDPLParser::parse(Rule::program, "123\nhi").unwrap();
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        eprintln!("ERROR: Please provide an LDPL file.");
+        std::process::exit(1);
+    }
+    let file = args.get(1).unwrap();
+    let code = std::fs::read_to_string(file).unwrap();
 
-    // Because ident_list is silent, the iterator will contain idents
+    let pairs = LDPLParser::parse(Rule::program, &code).unwrap();
     for pair in pairs {
         // A pair is a combination of the rule which matched and a span of input
         println!("Rule:    {:?}", pair.as_rule());
         println!("Span:    {:?}", pair.as_span());
         println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
-                Rule::ident => println!("Ident:   {}", inner_pair.as_str()),
-                rule => println!("{:?}:   {}", rule, inner_pair.as_str()),
-            };
-        }
     }
 }
