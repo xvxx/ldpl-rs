74251ccda6c48c0cbf7214e0ee5424747f575b65
chris west <c@xvxx.io>
2020-04-25 14:32:50 -0700

    WHILE and SUB

--- src/emitter.rs
+++ src/emitter.rs
@@ -2,21 +2,66 @@
 
 use crate::{parser::Rule, LDPLResult};
 use pest::iterators::{Pair, Pairs};
+/// Setup the C++ main() function
+///
+const MAIN_HEADER: &'static str = r#"
+ldpl_list<chText> VAR_ARGV;
+ldpl_number VAR_ERRORCODE = 0;
+chText VAR_ERRORTEXT = "";
 
+int main(int argc, char* argv[]) {
+    cout.precision(numeric_limits<ldpl_number>::digits10);
+    for(int i = 1; i < argc; ++i) VAR_ARGV.inner_collection.push_back(argv[i]);
+"#;
+
+const MAIN_FOOTER: &'static str = r#"
+    return 0;
+}
+"#;
+
+/// Turns parsed LDPL code into a string of C++ code.
 pub fn emit(ast: Pairs<Rule>) -> LDPLResult<String> {
-    let mut out = String::new();
+    let mut out = vec![];
+    let mut main = MAIN_HEADER.to_string();
+
     for pair in ast {
         match pair.as_rule() {
             Rule::cxx_ext_stmt => todo!(),
-            Rule::data_section => out.push_str(&emit_data(pair)?),
-            Rule::procedure_section => {}
+            Rule::data_section => out.push(emit_data(pair)?),
+            Rule::create_stmt_stmt => todo!(),
+            Rule::sub_def_stmt => out.push(emit_sub_def_stmt(pair)?),
             Rule::EOI => {}
-            _ => {
-                return error!("Unexpected Rule: {:?}", pair);
-            }
+            _ => main.push_str(&emit_subproc_stmt(pair)?),
         }
     }
-    Ok(out)
+
+    main.push_str(MAIN_FOOTER);
+    out.push(main);
+
+    Ok(out.join("\n"))
+}
+
+/// LDPL Type => C++ Type
+fn type_for(ldpl_type: &str) -> &str {
+    match ldpl_type.to_lowercase().as_ref() {
+        "number" => "ldpl_number",
+        "number list" => "ldpl_list<ldpl_number>",
+        "number map" => "ldpl_map<chText>",
+        "text" => "chText",
+        "text list" => "ldpl_list<chText>",
+        "text map" => "ldpl_map<chText>",
+        _ => "UNKNOWN_TYPE",
+    }
+}
+
+/// Mangle a variable name for C++.
+fn mangle_var(ident: &str) -> String {
+    format!("VAR_{}", mangle(ident))
+}
+
+/// Mangle a subprocedure name.
+fn mangle_sub(ident: &str) -> String {
+    format!("SUB_{}", mangle(ident))
 }
 
 /// Convert an ident to a C++-friendly ident by stripping illegal
@@ -34,7 +79,7 @@ fn mangle(ident: &str) -> String {
         }
     }
 
-    format!("VAR_{}", mangled)
+    mangled
 }
 
 /// Convert `name IS TEXT` into a C++ variable declaration.
@@ -44,37 +89,154 @@ fn emit_data(pair: Pair<Rule>) -> LDPLResult<String> {
 
     for def in pair.into_inner() {
         assert!(def.as_rule() == Rule::type_def);
+
         let mut parts = def.into_inner();
-        let ident = parts.next().unwrap();
-        let r#type = parts.next().unwrap();
-        match r#type.as_str().to_lowercase().as_ref() {
-            "number" => {
-                out.push(format!(r#"ldpl_number {} = 0;"#, mangle(ident.as_str())));
-            }
-            "text" => {
-                out.push(format!(r#"chText {} = "";"#, mangle(ident.as_str())));
-            }
-            "number list" => {
-                out.push(format!(
-                    r#"ldpl_list<ldpl_number> {};"#,
-                    mangle(ident.as_str())
-                ));
-            }
-            "number map" => {
-                out.push(format!(
-                    r#"ldpl_map<ldpl_number> {};"#,
-                    mangle(ident.as_str())
-                ));
-            }
-            "text list" => {
-                out.push(format!(r#"ldpl_list<chText> {};"#, mangle(ident.as_str())));
-            }
-            "text map" => {
-                out.push(format!(r#"ldpl_map<chText> {};"#, mangle(ident.as_str())));
-            }
-            _ => todo!(),
+        let ident = parts.next().unwrap().as_str();
+        let typename = parts.next().unwrap().as_str();
+        let mut var = format!("{} {}", type_for(typename), mangle_var(ident));
+
+        if typename == "number" {
+            var.push_str(" = 0");
+        } else if typename == "text" {
+            var.push_str(r#" = """#);
         }
+
+        var.push(';');
+        out.push(var);
     }
 
     Ok(out.join("\n"))
+}
+
+/// Convert a param list into a C++ function signature params list.
+fn emit_params(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut out = vec![];
+
+    for def in pair.into_inner() {
+        assert!(def.as_rule() == Rule::type_def);
+        let mut parts = def.into_inner();
+        let ident = parts.next().unwrap().as_str();
+        let typename = parts.next().unwrap().as_str();
+        out.push(format!("{} {}", type_for(typename), mangle_var(ident),));
+    }
+
+    Ok(out.join(", "))
+}
+
+/// Function definition.
+fn emit_sub_def_stmt(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut params = String::new();
+    let mut name = String::new();
+    let mut vars = String::new();
+    let mut body: Vec<String> = vec![];
+
+    for node in pair.into_inner() {
+        match node.as_rule() {
+            Rule::ident => {
+                name = mangle_sub(node.as_str());
+            }
+            Rule::sub_param_section => params = emit_params(node)?,
+            Rule::sub_data_section => vars = emit_data(node)?,
+            _ => body.push(emit_subproc_stmt(node)?),
+        }
+    }
+
+    Ok(format!(
+        "void {}({}) {{\n{}\n{}\n}}\n",
+        name,
+        params,
+        vars,
+        body.join("\n"),
+    ))
+}
+
+/// Emit a stmt from the PROCEDURE: section of a file or function.
+fn emit_subproc_stmt(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut out = vec![];
+
+    out.push(match pair.as_rule() {
+        Rule::call_stmt => "".to_string(),
+        Rule::display_stmt => "".to_string(),
+        Rule::store_stmt => emit_store_stmt(pair)?,
+        Rule::solve_stmt => "".to_string(),
+        Rule::if_stmt => "".to_string(),
+        Rule::else_stmt => "".to_string(),
+        Rule::while_stmt => emit_while_stmt(pair)?,
+        _ => "".to_string(),
+    });
+
+    Ok(out.join("\n"))
+}
+
+/// STORE _ IN _
+fn emit_store_stmt(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut iter = pair.into_inner();
+    let val = iter.next().unwrap().as_str();
+    let ident = iter.next().unwrap().as_str();
+    Ok(format!("{} = {};\n", mangle_var(ident), val))
+}
+
+/// IF and WHILE test statement
+fn emit_test_stmt(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut out = vec![];
+    for test in pair.into_inner() {
+        match test.as_rule() {
+            Rule::or_test_expr => {
+                let mut iter = test.into_inner();
+                let left = emit_test_expr(iter.next().unwrap())?;
+                let right = emit_test_expr(iter.next().unwrap())?;
+                out.push(format!("({} || {})", left, right));
+            }
+            Rule::and_test_expr => {
+                let mut iter = test.into_inner();
+                let left = emit_test_expr(iter.next().unwrap())?;
+                let right = emit_test_expr(iter.next().unwrap())?;
+                out.push(format!("({} && {})", left, right));
+            }
+            Rule::one_test_expr => out.push(emit_test_expr(test)?),
+            _ => unimplemented!(),
+        }
+    }
+    Ok(out.join("\n"))
+}
+
+/// Single test expression. Use _stmt for expressions with OR / AND.
+fn emit_test_expr(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut iter = pair.into_inner();
+    let left = emit_expr(iter.next().unwrap())?;
+    let sign = match iter.next().unwrap().as_rule() {
+        Rule::equal_expr => "==",
+        Rule::not_equal_expr => "!=",
+        Rule::gt_expr => ">",
+        Rule::lt_expr => "<",
+        Rule::gte_expr => ">=",
+        Rule::lte_expr => "<=",
+        _ => unimplemented!(),
+    };
+    let right = emit_expr(iter.next().unwrap())?;
+    Ok(format!("({} {} {})", left, sign, right))
+}
+
+/// Variable, Number, or Text
+fn emit_expr(pair: Pair<Rule>) -> LDPLResult<String> {
+    Ok(match pair.as_rule() {
+        Rule::var => mangle_var(pair.as_str()),
+        Rule::number => pair.as_str().to_string(),
+        Rule::text => pair.as_str().to_string(),
+        Rule::linefeed => "\n".to_string(),
+        _ => unimplemented!(),
+    })
+}
+
+/// WHILE _ DO REPEAT
+fn emit_while_stmt(pair: Pair<Rule>) -> LDPLResult<String> {
+    let mut iter = pair.into_inner();
+    let test = iter.next().unwrap();
+    let test = emit_test_stmt(test)?;
+
+    let mut body = vec![];
+    for node in iter.next().unwrap().into_inner() {
+        body.push(emit_subproc_stmt(node)?);
+    }
+    Ok(format!("while {} {{\n{}\n}}\n", test, body.join("\n")))
 }
